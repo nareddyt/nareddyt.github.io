1c65ce51caadd6108b93feab41c5600b0597aa54
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2017-01-30 23:57:49 -0500

    Update to new theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
@@ -17,10 +18,10 @@ var banner = ['/*!\n',
 ].join('');
 
 // Compile LESS files from /less into /css
-gulp.task('less', function () {
-    return gulp.src('less/freelancer.less')
+gulp.task('less', function() {
+    return gulp.src('less/agency.less')
         .pipe(less())
-        .pipe(header(banner, {pkg: pkg}))
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
@@ -28,10 +29,10 @@ gulp.task('less', function () {
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function () {
-    return gulp.src('css/freelancer.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename({suffix: '.min'}))
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('css/agency.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
@@ -39,11 +40,11 @@ gulp.task('minify-css', ['less'], function () {
 });
 
 // Minify JS
-gulp.task('minify-js', function () {
-    return gulp.src('js/freelancer.js')
+gulp.task('minify-js', function() {
+    return gulp.src('js/agency.js')
         .pipe(uglify())
-        .pipe(header(banner, {pkg: pkg}))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
             stream: true
@@ -51,42 +52,54 @@ gulp.task('minify-js', function () {
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function () {
+gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'));;;;;;;;;;;;;;;;;;;;;;;;;;;
+        .pipe(gulp.dest('vendor/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'));;;;;;;;;;;;;;;;;;;;;;;;;;;
+        .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src([
-        'node_modules/font-awesome/**',
-        '!node_modules/font-awesome/**/*.map',
-        '!node_modules/font-awesome/.npmignore',
-        '!node_modules/font-awesome/*.txt',
-        '!node_modules/font-awesome/*.md',
-        '!node_modules/font-awesome/*.json'
-    ])
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
         .pipe(gulp.dest('vendor/font-awesome'))
-});
+})
 
 // Run everything
 gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function () {
+gulp.task('browserSync', function() {
     browserSync.init({
         server: {
             baseDir: ''
         },
     })
-});;;;;;;;;;;;;;;;;;;;;;;;;;;
+})
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function () {
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+// Compiles SCSS files from /scss into /css
+// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
+gulp.task('sass', function() {
+    return gulp.src('scss/agency.scss')
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
