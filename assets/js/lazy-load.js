2d2432bde71ba1a67ae9b08315e2d8314a0a4c5a
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-05-01 18:48:36 -0700

    Lazy load linkedin badge as well

--- assets/js/lazy-load.js
+++ assets/js/lazy-load.js
@@ -1,6 +1,13 @@
 (function($) {
 
   $(window).load(function () {
+    $('#linkedin-badge').html('' +
+      '<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="tejasvinareddyteju">' +
+      '    <a class="LI-simple-link" href=\'https://www.linkedin.com/in/tejasvinareddyteju?trk=profile-badge\'>' +
+      '        Tejasvi Nareddy' +
+      '    </a>' +
+      '</div>\n');
+
     $('#github-contributions').after('' +
       '<a href="https://github.com/nareddyt" target="_blank">\n' +
       '    <img src="https://ghchart.rshah.org/nareddyt" alt="nareddyt\'s Github Contributions" />\n' +
